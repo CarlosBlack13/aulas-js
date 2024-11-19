e532c9bd9f982b0497cdc2801d61985711a7efd9
CarlosBlack13 <163689188+CarlosBlack13@users.noreply.github.com>
2024-11-19 19:23:45 -0300

    Add files via upload

---
+++ js/exercícios/exemplo10/acaos.js
@@ -0,0 +1,18 @@
+let res = window.document.getElementById("saida")
+
+
+function acao1(){
+    res.innerHTML = `voce clicou no botao 1`
+}
+
+function acao2(){
+    res.innerHTML = `voce clicou no botao 2`
+}
+
+function acao3(){
+    res.innerHTML = `voce clicou no botao 3`
+}
+
+function acao4(){
+    res.innerHTML = `voce clicou no botao 4`
+}
